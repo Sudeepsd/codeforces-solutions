8a882bcd6d3b15a2cf92b0b06ff29ebaa7271c4d
Sudeepsd <53992202+Sudeepsd@users.noreply.github.com>
2026-02-10 16:14:16 +0530

    Update 1030A.cpp

--- 1000-1250/1030A.cpp
+++ 1000-1250/1030A.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n; cin >> n;
+
+    bool hard = false;
+    for (int i = 0; i < n; ++i) {
+        int x; cin >> x;
+        if (x == 1)
+            hard = true;
+    }
+    if (hard)
+        cout << "HARD";
+    else
+        cout << "EASY";
+
+    return 0;
+}
